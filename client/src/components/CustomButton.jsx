17ce2b39a774c31be6e3818ad19ff8488f026e5a
roblebob <robert.koppisch@gmail.com>
2024-05-03 16:40:17 +0200

    finished HomePage @36:50

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
-const CustomButton = () => {
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
+    }
+  };
+
   return (
-    <div>CustomButton</div>
-  )
-}
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
-import {CustomButton} from "../components";
+import { CustomButton } from "../components";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -33,7 +33,10 @@ const Home = () => {
               </h1>
             </motion.div>
 
-            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
               <p className="max-w-md font-normal text-grey-600 text-base">
                 Create your unique and exclusive shirt with our brand-new 3D
                 customization tool.
@@ -42,12 +45,11 @@ const Home = () => {
               </p>
 
               <CustomButton
-                type="filled" 
+                type="filled"
                 title="Customize It"
-                handleClick={() => state.intro = false}
+                handleClick={() => (state.intro = false)}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-                />
-
+              />
             </motion.div>
           </motion.div>
         </motion.section>
