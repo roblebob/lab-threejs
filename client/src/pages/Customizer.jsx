cfeac20d5c9ce4d813f2060b6ce78bab4b2ae182
roblebob <robert.koppisch@gmail.com>
2024-05-04 18:24:44 +0200

    @42:58

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import {
+  AIPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tab,
+} from "../components";
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default Customizer
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div 
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            >
+
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Customizer;
